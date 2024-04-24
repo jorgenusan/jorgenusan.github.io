d12bf90d68c9cba02a30f953e606d594bb04df19
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-24 14:51:25 +0200

    Add tailwind typography plugin and import remark-rehype

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import remarkRehype from 'remark-rehype';
 
 // https://astro.build/config
 export default defineConfig({
