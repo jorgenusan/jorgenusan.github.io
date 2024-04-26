9be09d5ce8d63834fa718aa86b3eb0293b07e029
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-26 18:24:07 +0200

    Add new background images and custom colors to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+				  "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			  },
+			  colors: {
+				'custom-blue': '#747fff',
+				'custom-black': '#1d232a',
+			  },
+		},
 	},
 	plugins: [require("daisyui"), require("@tailwindcss/typography")],
 	daisyui: {
