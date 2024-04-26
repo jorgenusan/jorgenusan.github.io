49b82e2c23289bb99ebbdddea89cc599f3ea0fa3
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-26 12:28:05 +0200

    Update translations and navbar links

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,6 @@ export default {
 	},
 	plugins: [require("daisyui"), require("@tailwindcss/typography")],
 	daisyui: {
-		themes: ["dracula", "bumblebee", "cupcake"],
+		themes: ["dracula", "light", "cupcake"],
 	},
 }
