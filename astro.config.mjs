bc31de8b316de2930abb491042cfb48d2b042422
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-30 19:29:10 +0200

    Add @astrojs/mdx dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
     routing: {
       prefixDefaultLocale: false
     }
-  },
+  }
 });
